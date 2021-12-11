f9b3f677a63971eb2073225901c317847de42f95
João Chirindza <johnchirindza@gmail.com>
2021-12-11 12:47:13 +0200

    Update App.js | Use Routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';  
+import React from "react"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import Countries from "./components/Countries"
+import Country from "./components/Country"
+import Header from "./components/Header"
 
-function App() { 
-    return (  
-      <div>
-        <h1>Rest Countries API</h1>
-      </div>
-    );  
-}  
-  
-export default App; 
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Route exact path='/'>
+        <Countries />
+      </Route>
+
+      <Route path="/countries/:name" children={<Country />}></Route>
+    </Router>
+  )
+}
+export default App
