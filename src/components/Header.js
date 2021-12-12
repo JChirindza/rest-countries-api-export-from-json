d055a278efdae35757c886f36ffd834e858848cf
João Chirindza <johnchirindza@gmail.com>
2021-12-12 13:43:46 +0200

    Update Header.js | UI update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,23 @@
 import React from 'react'
 
 const Header = () => {
-  return (
-    <>
-        <header className="header">
-            <div className="container">
-                <ul>
-                    <div className="item-left">
-                        <li><a className="" href="/"><i className="fas fa-globe"></i> World Countries</a></li>
-                    </div>
-                    <div className="item-right">
-                        <li><a href="#"><i className="fas fa-external-link-alt"></i> Live demo</a></li>
-                        <li><a href="#"><i className="fab fa-github"></i> Github repo</a></li>
-                    </div>
-                </ul>
-            </div>
-        </header>
-    </>
-  )
+    return (
+        <>
+            <header className="header">
+                <div className="container">
+                    <ul>
+                        <div className="item item-left">
+                            <li><a className="" href="/"><i className="fas fa-globe"></i> <span>World Countries</span></a></li>
+                        </div>
+                        <div className="item item-right">
+                            <li><a href="#"><i className="fas fa-search"></i> <span>Search</span></a></li>
+                            <li><a href="#"><i className="fab fa-github"></i> <span>Github repo</span></a></li>
+                        </div>
+                    </ul>
+                </div>
+            </header>
+        </>
+    )
 }
 
 export default Header
