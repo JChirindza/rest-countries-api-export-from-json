c43fc2750fd3ca0329da8360da82a98244bedcf5
João Chirindza <johnchirindza@gmail.com>
2021-12-13 14:03:27 +0200

    Update App.js | change Route path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import Countries from "./components/Countries"
 import Country from "./components/Country"
 import Header from "./components/Header"
+// import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
         <Countries />
       </Route>
 
-      <Route path="/countries/:name" children={<Country />}></Route>
+      <Route path="/country/:name" children={<Country />}></Route>
+      {/* <Footer /> */}
     </Router>
   )
 }
-export default App
+export default App;
