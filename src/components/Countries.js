36da1f537ea4bdea40eeec0d7bf8ff6b80f4181a
João Chirindza <johnchirindza@gmail.com>
2021-12-17 04:35:52 +0200

    Update countries and country UI
    Remove react-json-to-csv dependence from packages.
    
    Change to countries api v3.1
    
    Create download functionality to export data to difrent file types.

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,78 +1,90 @@
-import React from 'react'
-import {useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-const path = 'https://restcountries.com/v2/all'
+const path = "https://restcountries.com/v3.1/all";
 
 const Countries = () => {
+    const [countries, setCountries] = useState([]);
 
-    const [countries, setCountries ] = useState([])
-
-    const fetchCountryData = async() => {
-        const res = await fetch(path)
-        const countries = await res.json()
-        setCountries(countries)
-    }
+    const fetchCountryData = async () => {
+        const res = await fetch(path);
+        const countries = await res.json();
+        setCountries(countries);
+    };
 
     useEffect(() => {
-        fetchCountryData()
-    }, [])
+        fetchCountryData();
+    }, []);
 
     return (
         <>
             <div className="countries">
-                <div className="countries-title"><h1 className="item-center">World Countries</h1></div>
-                
-                <div className="grid">
-                    {countries.map((country) => {
-                        const {
-                            numericCode, 
-                            flag,
-                            name,
-                            capital,
-                            region,
-                            subregion,
-                            population,
-                            area,
-                            timezones,
-                            nativeName
-                        } = country
+                <div className="container">
+                    <div className="countries-title">
+                        <h1 className="item-center">All World Countries</h1>
+                    </div>
+                    <div className="grid">
+                        {countries.map((country) => {
+                            const {
+                                ccn3,
+                                flags,
+                                name,
+                                capital,
+                                region,
+                                subregion,
+                                population,
+                                area,
+                                timezones,
+                                nativeName,
+                            } = country;
 
-                        return (
-                            <article key={numericCode}>
-                                <div className="image">
-                                    <img src={flag} alt={name} />
-                                </div>
-                                <div className="text">
-                                    <h2>{name}</h2>
-                                    <h3>Capital: <span>{capital}</span></h3>
-                                    <h3>Regiao: <span>{region}</span></h3>
-                                    <h3>Sub-regiao: <span>{subregion}</span></h3>
-                                    <h3>Populacao: <span>{population}</span></h3>
-                                    <h3>Area: <span>{area} Km<sup>2</sup></span></h3>
-                                    <div className="timezones">
-                                        <h3>Fuso horario: </h3>
-                                        {
-                                            timezones.map((timezone) => {
-                                                return (
-                                                <ul key={timezone}>
-                                                    <li>{timezone}</li>
-                                                </ul>
-                                                )
-                                            })
-                                        }
+                            return (
+                                <article key={ccn3}>
+                                    <div className="image">
+                                        <img
+                                            src={flags.svg}
+                                            alt={name.common}
+                                        />
                                     </div>
-                                    
-                                    <h3>Nome nativo: <span>{nativeName}</span></h3>
-                                    <Link className="button" to={`/${name}`}><i className="fas fa-eye"></i> View More</Link>
-                                </div>
-                            </article>
-                        )
-                    })}
+                                    <div className="text">
+                                        <h2>{name.common}</h2>
+                                        <h3>
+                                            Capital: <span>{capital}</span>
+                                        </h3>
+                                        <h3>
+                                            Region: <span>{region}</span>
+                                        </h3>
+                                        <h3>
+                                            Subregion: <span>{subregion}</span>
+                                        </h3>
+                                        <h3>
+                                            Population:{" "}
+                                            <span>{population}</span>
+                                        </h3>
+                                        <h3>
+                                            Area:{" "}
+                                            <span>
+                                                {area} Km<sup>2</sup>
+                                            </span>
+                                        </h3>
+
+                                        <Link
+                                            className="button"
+                                            to={`country/${name.common}`}
+                                        >
+                                            <i className="fas fa-eye"></i> View
+                                            More
+                                        </Link>
+                                    </div>
+                                </article>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Countries
+export default Countries;
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,68 +1,118 @@
-import React, { useState, useEffect } from "react"
-import { Link, useParams } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import exportFromJSON from "export-from-json";
+
+function download(exportType, data, countryName) {
+    const fileName = countryName;
+    const fields = [
+        "ccn3",
+        "flags",
+        "name",
+        "nativeName",
+        "population",
+        "region",
+        "subregion",
+        "capital",
+        "tld",
+        "currencies",
+        "languages",
+        "borders",
+        "timezones",
+    ];
+
+    exportFromJSON({ data, fileName, fields, exportType });
+}
 
 const Country = () => {
-  const [country, setCountry] = useState([])
-  const { name } = useParams()
+    const [country, setCountry] = useState([]);
+    const { name } = useParams();
 
-  useEffect(() => {
-    const fetchCountryData = async () => {
-        const res = await fetch(`https://restcountries.com/v3.1/name/${name}`)
-        const country = await res.json()
-        setCountry(country)
-    }
+    useEffect(() => {
+        const fetchCountryData = async () => {
+            const res = await fetch(
+                `https://restcountries.com/v3.1/name/${name}`
+            );
+            const country = await res.json();
+            setCountry(country);
+        };
 
-    fetchCountryData()
-  }, [name])
+        fetchCountryData();
+    }, [name]);
 
-  return (
-    <>
-      <section className="country">
-        <Link to="/" className="btn btn-light">
-            <i className="fas fa-arrow-left 2x" title="Back home"></i>
-        </Link>
-        {country.map((c) => {
-          const {
-            numericCode,
-            flags,
-            name,
-            nativeName,
-            population,
-            region,
-            subregion,
-            capital,
-            tld,
-            currencies,
-            languages,
-            borders,
-            timezones
-          } = c
+    return (
+        <>
+            <section className="country">
+                <div>
+                    <Link to="/" className="btn btn-back">
+                        <i
+                            className="fas fa-arrow-left 2x"
+                            title="Back home"
+                        ></i>
+                    </Link>
 
-          return (
-            <article key={numericCode}>
-               
-
-                <div className="country-name">
-                    <h2>{name.official}</h2>
-                </div>
-
-                <div className="country-inner">
-                    
-                    <div className="flag">
-                        <img src={flags.svg} alt={name.common} />
+                    <div className="go-to-downloads">
+                        <i className="fas fa-download"></i>
                     </div>
+                </div>
+                {country.map((c) => {
+                    const {
+                        ccn3,
+                        flags,
+                        name,
+                        nativeName,
+                        population,
+                        region,
+                        subregion,
+                        capital,
+                        tld,
+                        currencies,
+                        languages,
+                        borders,
+                        timezones,
+                    } = c;
 
-                    <div className="country-details">
-                        <div>
-                            <h5>Native Name: <span>{nativeName}</span></h5>
-                            <h5>Capital: <span>{capital}</span>{" "}</h5>
-                            <h5>Region: <span>{region}</span></h5>
-                            <h5>Sub Region: <span>{subregion}</span></h5>
-                            <h5>Population: <span>{population.toLocaleString()}</span></h5>
-                        </div>
-                        <div>
-                            <h5>Top Level Domain: <span>{tld}</span></h5>
-                            {/* <h5>Currencies: 
+                    return (
+                        <article key={ccn3}>
+                            <div className="country-name">
+                                <h2>{name.official}</h2>
+                            </div>
+
+                            <div className="country-inner">
+                                <div className="flag">
+                                    <img src={flags.svg} alt={name.common} />
+                                </div>
+
+                                <div className="country-details">
+                                    <div>
+                                        <h5>
+                                            Native Name:{" "}
+                                            <span>
+                                                {
+                                                    // c["name"].['nativeName'].getByIndex(0).common
+                                                }
+                                            </span>
+                                        </h5>
+                                        <h5>
+                                            Capital: <span>{capital}</span>{" "}
+                                        </h5>
+                                        <h5>
+                                            Region: <span>{region}</span>
+                                        </h5>
+                                        <h5>
+                                            Sub Region: <span>{subregion}</span>
+                                        </h5>
+                                        <h5>
+                                            Population:{" "}
+                                            <span>
+                                                {population.toLocaleString()}
+                                            </span>
+                                        </h5>
+                                    </div>
+                                    <div>
+                                        <h5>
+                                            Top Level Domain: <span>{tld}</span>
+                                        </h5>
+                                        {/* <h5>Currencies: 
                                 <span>
                                     {
                                         currencies.map((cur) => {
@@ -75,7 +125,7 @@ const Country = () => {
                                     }
                                 </span>
                             </h5> */}
-                            {/* <h5>Languages: 
+                                        {/* <h5>Languages: 
                                 <span>
                                     {
                                         languages.map((lang) => {
@@ -88,51 +138,106 @@ const Country = () => {
                                     }
                                 </span>
                             </h5> */}
-                        </div>
-                    </div>
-                </div>
+                                    </div>
+                                </div>
+                            </div>
 
-                <div>
-                    <h3>Border Countries: </h3>
-                    <div className="borders">
-                        {borders ? 
-                        borders.map((border) => {
-                            return (
-                            <ul key={border}>
-                                <li>{border}</li>
-                            </ul>
-                            )
-                        })
-                        : 
-                        <div>
-                            <ul>
-                                <li>No Border</li>
-                            </ul>
-                        </div>
-                        }
-                    </div>
-                </div>
+                            <div>
+                                <h3>Border Countries: </h3>
+                                <div className="borders">
+                                    {borders ? (
+                                        borders.map((border) => {
+                                            return (
+                                                <ul key={border}>
+                                                    <li>{border}</li>
+                                                </ul>
+                                            );
+                                        })
+                                    ) : (
+                                        <div>
+                                            <ul>
+                                                <li>No Border</li>
+                                            </ul>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
 
-                <div>
-                    <h3>Timezones: </h3>
-                    <div className="borders">
-                        {
-                            timezones.map((timezone) => {
-                                return (
-                                <ul key={timezone}>
-                                    <li>{timezone}</li>
-                                </ul>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-            </article>
-          )
-        })}
-      </section>
-    </>
-  )
-}
+                            <div>
+                                <h3>Timezones: </h3>
+                                <div className="borders">
+                                    {timezones.map((timezone) => {
+                                        return (
+                                            <ul key={timezone}>
+                                                <li>{timezone}</li>
+                                            </ul>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                            <hr />
+                            {/* Downloads */}
+                            <div className="downloads" id="downloads">
+                                <div className="text">
+                                    <h3 className="item-center">
+                                        Export/Download files
+                                    </h3>
+                                    <h4 className="text-muted item-center">
+                                        <span> .csv; .xls;.xml;</span>
+                                    </h4>
+                                </div>
 
-export default Country
+                                <div className="download-btn">
+                                    <div className="d-grid justify-content-center">
+                                        <h4 className="d-flex justify-content-center">
+                                            Export File:
+                                        </h4>
+                                        <button
+                                            onClick={() =>
+                                                download("json", c, name.common)
+                                            }
+                                            className="btn btn-primary btn-sm"
+                                        >
+                                            <i className="fas fa-download"></i>{" "}
+                                            Download JSON file
+                                        </button>
+
+                                        <button
+                                            onClick={() =>
+                                                download("csv", c, name.common)
+                                            }
+                                            className="btn btn-primary btn-sm"
+                                        >
+                                            <i className="fas fa-download"></i>{" "}
+                                            Download CSV file
+                                        </button>
+                                        <button
+                                            onClick={() =>
+                                                download("xls", c, name.common)
+                                            }
+                                            className="btn btn-primary btn-sm"
+                                        >
+                                            <i className="fas fa-download"></i>{" "}
+                                            Download XLS file(Excel)
+                                        </button>
+                                        <button
+                                            onClick={() =>
+                                                download("xml", c, name.common)
+                                            }
+                                            className="btn btn-primary btn-sm"
+                                        >
+                                            <i className="fas fa-download"></i>{" "}
+                                            Download XML file
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                    );
+                })}
+            </section>
+        </>
+    );
+};
+
+export default Country;
