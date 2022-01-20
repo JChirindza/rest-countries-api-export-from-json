5af1c0e82c13c71f9df068407678c4e4fa2a1207
João Chirindza <johnchirindza@gmail.com>
2022-01-20 18:54:36 +0200

    deploying react application on Heroku

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
     return (
@@ -7,17 +7,35 @@ const Header = () => {
                 <div className="container">
                     <ul>
                         <div className="item item-left">
-                            <li><a className="nav-logo" href="/"><i className="fas fa-globe"></i> <span>World Countries</span></a></li>
+                            <li>
+                                <a className="nav-logo" href="/">
+                                    <i className="fas fa-globe"></i>{" "}
+                                    <span>World Countries</span>
+                                </a>
+                            </li>
                         </div>
                         <div className="item item-right">
-                            <li><a href="#"><i className="fas fa-search"></i> <span>Search</span></a></li>
-                            <li><a href="#"><i className="fab fa-github"></i> <span>Github repo</span></a></li>
+                            <li>
+                                <a href="#">
+                                    <i className="fas fa-search"></i>{" "}
+                                    <span>Search</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://github.com/JChirindza/rest-countries-api-export-from-json-react"
+                                    target="_blank"
+                                >
+                                    <i className="fab fa-github"></i>{" "}
+                                    <span>Github repo</span>
+                                </a>
+                            </li>
                         </div>
                     </ul>
                 </div>
             </header>
         </>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
