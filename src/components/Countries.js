cebc1b3fbf5672f5cc8bc1747b4875836010a2c6
João Chirindza <johnchirindza@gmail.com>
2022-01-30 19:38:46 +0200

    Add Loading Spinner

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
+import spinner from "./img/spinner.gif";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Loading from "./Loading";
 
 const path = "https://restcountries.com/v3.1/all";
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     const fetchCountryData = async () => {
         const res = await fetch(path);
         const countries = await res.json();
         setCountries(countries);
+        setIsLoading(false);
     };
 
     useEffect(() => {
@@ -24,63 +28,69 @@ const Countries = () => {
                     <div className="countries-title">
                         <h1 className="item-center">All World Countries</h1>
                     </div>
-                    <div className="grid">
-                        {countries.map((country) => {
-                            const {
-                                ccn3,
-                                flags,
-                                name,
-                                capital,
-                                region,
-                                subregion,
-                                population,
-                                area,
-                                timezones,
-                                nativeName,
-                            } = country;
 
-                            return (
-                                <article key={ccn3}>
-                                    <div className="image">
-                                        <img
-                                            src={flags.svg}
-                                            alt={name.common}
-                                        />
-                                    </div>
-                                    <div className="text">
-                                        <h2>{name.common}</h2>
-                                        <h3>
-                                            Capital: <span>{capital}</span>
-                                        </h3>
-                                        <h3>
-                                            Region: <span>{region}</span>
-                                        </h3>
-                                        <h3>
-                                            Subregion: <span>{subregion}</span>
-                                        </h3>
-                                        <h3>
-                                            Population:{" "}
-                                            <span>{population}</span>
-                                        </h3>
-                                        <h3>
-                                            Area:{" "}
-                                            <span>
-                                                {area} Km<sup>2</sup>
-                                            </span>
-                                        </h3>
+                    {!isLoading ? (
+                        <div className="grid">
+                            {countries.map((country) => {
+                                const {
+                                    ccn3,
+                                    flags,
+                                    name,
+                                    capital,
+                                    region,
+                                    subregion,
+                                    population,
+                                    area,
+                                    timezones,
+                                    nativeName,
+                                } = country;
 
-                                        <Link
-                                            className="button"
-                                            to={`country/${name.common}`}
-                                        >
-                                            <i className="fas fa-eye"></i> View
-                                            More
-                                        </Link>
-                                    </div>
-                                </article>
-                            );
-                        })}
-                    </div>
+                                return (
+                                    <article key={ccn3}>
+                                        <div className="image">
+                                            <img
+                                                src={flags.svg}
+                                                alt={name.common}
+                                            />
+                                        </div>
+                                        <div className="text">
+                                            <h2>{name.common}</h2>
+                                            <h3>
+                                                Capital: <span>{capital}</span>
+                                            </h3>
+                                            <h3>
+                                                Region: <span>{region}</span>
+                                            </h3>
+                                            <h3>
+                                                Subregion:{" "}
+                                                <span>{subregion}</span>
+                                            </h3>
+                                            <h3>
+                                                Population:{" "}
+                                                <span>{population}</span>
+                                            </h3>
+                                            <h3>
+                                                Area:{" "}
+                                                <span>
+                                                    {area} Km<sup>2</sup>
+                                                </span>
+                                            </h3>
+
+                                            <Link
+                                                className="button"
+                                                to={`country/${name.common}`}
+                                            >
+                                                <i className="fas fa-eye"></i>{" "}
+                                                View More
+                                            </Link>
+                                        </div>
+                                    </article>
+                                );
+                            })}
+                        </div>
+                    ) : (
+                        <Loading />
+                    )}
                 </div>
             </div>
         </>
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import exportFromJSON from "export-from-json";
+import Loading from "./Loading";
 
 function download(exportType, data, countryName) {
     const fileName = countryName;
@@ -26,6 +27,7 @@ function download(exportType, data, countryName) {
 const Country = () => {
     const [country, setCountry] = useState([]);
     const { name } = useParams();
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchCountryData = async () => {
@@ -34,6 +36,7 @@ const Country = () => {
             );
             const country = await res.json();
             setCountry(country);
+            setIsLoading(false);
         };
 
         fetchCountryData();
@@ -50,65 +53,68 @@ const Country = () => {
                         ></i>
                     </Link>
                 </div>
-                {country.map((c) => {
-                    const {
-                        ccn3,
-                        flags,
-                        name,
-                        nativeName,
-                        population,
-                        region,
-                        subregion,
-                        capital,
-                        tld,
-                        currencies,
-                        languages,
-                        borders,
-                        timezones,
-                    } = c;
-
-                    return (
-                        <article key={ccn3}>
-                            <div className="country-name">
-                                <h2>{name.official}</h2>
-                            </div>
-
-                            <div className="country-inner">
-                                <div className="flag">
-                                    <img src={flags.svg} alt={name.common} />
+                {!isLoading ? (
+                    country.map((c) => {
+                        const {
+                            ccn3,
+                            flags,
+                            name,
+                            nativeName,
+                            population,
+                            region,
+                            subregion,
+                            capital,
+                            tld,
+                            currencies,
+                            languages,
+                            borders,
+                            timezones,
+                        } = c;
+                        return (
+                            <article key={ccn3}>
+                                <div className="country-name">
+                                    <h2>{name.official}</h2>
                                 </div>
 
-                                <div className="country-details">
-                                    <div>
-                                        <h5>
-                                            Native Name:{" "}
-                                            <span>
-                                                {
-                                                    // c["name"].['nativeName'].getByIndex(0).common
-                                                }
-                                            </span>
-                                        </h5>
-                                        <h5>
-                                            Capital: <span>{capital}</span>{" "}
-                                        </h5>
-                                        <h5>
-                                            Region: <span>{region}</span>
-                                        </h5>
-                                        <h5>
-                                            Sub Region: <span>{subregion}</span>
-                                        </h5>
-                                        <h5>
-                                            Population:{" "}
-                                            <span>
-                                                {population.toLocaleString()}
-                                            </span>
-                                        </h5>
+                                <div className="country-inner">
+                                    <div className="flag">
+                                        <img
+                                            src={flags.svg}
+                                            alt={name.common}
+                                        />
                                     </div>
-                                    <div>
-                                        <h5>
-                                            Top Level Domain: <span>{tld}</span>
-                                        </h5>
-                                        {/* <h5>Currencies: 
+
+                                    <div className="country-details">
+                                        <div>
+                                            <h5>
+                                                Native Name:{" "}
+                                                <span>
+                                                    {/* {name.nativeName[0].common} */}
+                                                </span>
+                                            </h5>
+                                            <h5>
+                                                Capital: <span>{capital}</span>{" "}
+                                            </h5>
+                                            <h5>
+                                                Region: <span>{region}</span>
+                                            </h5>
+                                            <h5>
+                                                Sub Region:{" "}
+                                                <span>{subregion}</span>
+                                            </h5>
+                                            <h5>
+                                                Population:{" "}
+                                                <span>
+                                                    {population.toLocaleString()}
+                                                </span>
+                                            </h5>
+                                        </div>
+                                        <div>
+                                            <h5>
+                                                Top Level Domain:{" "}
+                                                <span>{tld}</span>
+                                            </h5>
+                                            {/* <h5>Currencies: 
                                 <span>
                                     {
                                         currencies.map((cur) => {
@@ -121,7 +127,7 @@ const Country = () => {
                                     }
                                 </span>
                             </h5> */}
-                                        {/* <h5>Languages: 
+                                            {/* <h5>Languages: 
                                 <span>
                                     {
                                         languages.map((lang) => {
@@ -134,101 +140,124 @@ const Country = () => {
                                     }
                                 </span>
                             </h5> */}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div>
-                                <h3>Border Countries: </h3>
-                                <div className="borders">
-                                    {borders ? (
-                                        borders.map((border) => {
+                                <div>
+                                    <h3>Border Countries: </h3>
+                                    <div className="borders">
+                                        {borders ? (
+                                            borders.map((border) => {
+                                                return (
+                                                    <ul key={border}>
+                                                        <li>{border}</li>
+                                                    </ul>
+                                                );
+                                            })
+                                        ) : (
+                                            <div>
+                                                <ul>
+                                                    <li>No Border</li>
+                                                </ul>
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <h3>Timezones: </h3>
+                                    <div className="borders">
+                                        {timezones.map((timezone) => {
                                             return (
-                                                <ul key={border}>
-                                                    <li>{border}</li>
+                                                <ul key={timezone}>
+                                                    <li>{timezone}</li>
                                                 </ul>
                                             );
-                                        })
-                                    ) : (
-                                        <div>
-                                            <ul>
-                                                <li>No Border</li>
-                                            </ul>
-                                        </div>
-                                    )}
-                                </div>
-                            </div>
-
-                            <div>
-                                <h3>Timezones: </h3>
-                                <div className="borders">
-                                    {timezones.map((timezone) => {
-                                        return (
-                                            <ul key={timezone}>
-                                                <li>{timezone}</li>
-                                            </ul>
-                                        );
-                                    })}
-                                </div>
-                            </div>
-                            <hr />
-                            {/* Downloads */}
-                            <div className="downloads" id="downloads">
-                                <div className="text">
-                                    <h3 className="item-center">
-                                        Export/Download files
-                                    </h3>
-                                    <h4 className="text-muted item-center">
-                                        <span> .xml; .json; .csv; .xls;</span>
-                                    </h4>
-                                </div>
-
-                                <div className="download-btn">
-                                    <div className="d-grid justify-content-center">
-                                        <button
-                                            onClick={() =>
-                                                download("xml", c, name.common)
-                                            }
-                                            className="btn btn-primary btn-sm"
-                                        >
-                                            <i className="fas fa-download"></i>{" "}
-                                            Download XML file
-                                        </button>
-
-                                        <button
-                                            onClick={() =>
-                                                download("json", c, name.common)
-                                            }
-                                            className="btn btn-primary btn-sm"
-                                        >
-                                            <i className="fas fa-download"></i>{" "}
-                                            Download JSON file
-                                        </button>
-
-                                        <button
-                                            onClick={() =>
-                                                download("csv", c, name.common)
-                                            }
-                                            className="btn btn-primary btn-sm"
-                                        >
-                                            <i className="fas fa-download"></i>{" "}
-                                            Download CSV file
-                                        </button>
-                                        <button
-                                            onClick={() =>
-                                                download("xls", c, name.common)
-                                            }
-                                            className="btn btn-primary btn-sm"
-                                        >
-                                            <i className="fas fa-download"></i>{" "}
-                                            Download XLS file(Excel)
-                                        </button>
+                                        })}
                                     </div>
                                 </div>
-                            </div>
-                        </article>
-                    );
-                })}
+
+                                {/* Downloads */}
+                                <div className="downloads" id="downloads">
+                                    <div className="text">
+                                        <h3 className="item-center">
+                                            Export/Download files
+                                        </h3>
+                                        <h4 className="text-muted item-center">
+                                            <span>
+                                                {" "}
+                                                .xml; .json; .csv; .xls;
+                                            </span>
+                                        </h4>
+                                    </div>
+
+                                    <div className="download-btn">
+                                        <div className="d-grid justify-content-center">
+                                            <button
+                                                onClick={() =>
+                                                    download(
+                                                        "xml",
+                                                        c,
+                                                        name.common
+                                                    )
+                                                }
+                                                className="btn btn-primary btn-sm"
+                                            >
+                                                <i className="fas fa-download"></i>{" "}
+                                                Download XML file
+                                            </button>
+
+                                            <button
+                                                onClick={() =>
+                                                    download(
+                                                        "json",
+                                                        c,
+                                                        name.common
+                                                    )
+                                                }
+                                                className="btn btn-primary btn-sm"
+                                            >
+                                                <i className="fas fa-download"></i>{" "}
+                                                Download JSON file
+                                            </button>
+
+                                            <button
+                                                onClick={() =>
+                                                    download(
+                                                        "csv",
+                                                        c,
+                                                        name.common
+                                                    )
+                                                }
+                                                className="btn btn-primary btn-sm"
+                                            >
+                                                <i className="fas fa-download"></i>{" "}
+                                                Download CSV file
+                                            </button>
+                                            <button
+                                                onClick={() =>
+                                                    download(
+                                                        "xls",
+                                                        c,
+                                                        name.common
+                                                    )
+                                                }
+                                                className="btn btn-primary btn-sm"
+                                            >
+                                                <i className="fas fa-download"></i>{" "}
+                                                Download XLS file(Excel)
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr />
+                            </article>
+                        );
+                    })
+                ) : (
+                    <Loading />
+                )}
             </section>
         </>
     );
