c080d080f4b516269f11e147a8fc68f7851683b7
João Chirindza <johnchirindza@gmail.com>
2022-01-31 23:58:54 +0200

    Create Filter.js

---
+++ src/components/Filter.js
@@ -0,0 +1,111 @@
+import React, { useEffect } from "react";
+
+const Filter = ({
+    searchInput,
+    setSearchInput,
+    setFiltered,
+    setCountries,
+    countries,
+}) => {
+    const regions = [
+        {
+            name: "Filter by region",
+            desc: "All",
+        },
+        {
+            name: "Africa",
+            desc: "Africa",
+        },
+        {
+            name: "Americas",
+            desc: "Americas",
+        },
+        {
+            name: "Asia",
+            desc: "Asia",
+        },
+        {
+            name: "Europe",
+            desc: "Europe",
+        },
+        {
+            name: "Oceania",
+            desc: "Oceania",
+        },
+    ];
+
+    // Prevent page reload when submitting the form
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    // Search countries
+    const searchCountries = (searchValue) => {
+        setSearchInput(searchValue);
+
+        if (searchInput) {
+            const filteredCountries = countries.filter((country) =>
+                Object.values(country)
+                    .join("")
+                    .toLowerCase()
+                    .includes(searchValue.toLowerCase())
+            );
+            setFiltered(filteredCountries);
+        } else {
+            setFiltered(countries);
+        }
+    };
+
+    // Filter by region
+
+    const filterRegions = async (region) => {
+        var url = `https://restcountries.com/v3.1/region/${region}`;
+
+        if (region === "") {
+            url = "https://restcountries.com/v3.1/all";
+        }
+
+        const res = await fetch(url);
+        const data = await res.json();
+        setCountries(data);
+    };
+
+    useEffect(() => {
+        filterRegions();
+        // eslint-disable-next-line
+    }, []);
+
+    return (
+        <>
+            <form class="filters" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    className="searchBox"
+                    id="searchBox"
+                    placeholder="Type to Search..."
+                    name="searchBox"
+                    autoComplete="off"
+                    onChange={(e) => searchCountries(e.target.value)}
+                />
+                <div>
+                    <select
+                        name="selectBox"
+                        id="selectBox"
+                        className="selectBox"
+                        onChange={(e) => filterRegions(e.target.value)}
+                        value={regions.name}
+                    >
+                        <option value="">All continents</option>
+                        <option value="Africa">Africa</option>
+                        <option value="Asia">Asia</option>
+                        <option value="Europe">Europe</option>
+                        <option value="Americas">Americas</option>
+                        <option value="Oceania">Oceania</option>
+                    </select>
+                </div>
+            </form>
+        </>
+    );
+};
+
+export default Filter;
