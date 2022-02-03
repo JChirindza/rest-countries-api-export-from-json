8ca23fb02e60c2b456770b49811989548a2d161a
João Chirindza <johnchirindza@gmail.com>
2022-02-03 20:47:52 +0200

    Implement Detect Offline/Online snackbar

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Countries from "./components/Countries";
 import Country from "./components/Country";
 import Header from "./components/Header";
+import NetDetector from "./components/NetDetector";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route exact path="/" element={<Countries />} />
-                <Route path="/country/:name" element={<Country />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route exact path="/" element={<Countries />} />
+                    <Route path="/country/:name" element={<Country />} />
+                </Routes>
+            </BrowserRouter>
+
+            <NetDetector />
+        </>
     );
 }
 export default App;
